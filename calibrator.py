d0f2072effec8d7fa21011be9ff0b89c09a19fbe
Huh, Jason (Contractor) <Jason.Huh@exchange.nordstrom.com>
2017-02-21 17:19:40 -0800

    initial commit

---
+++ calibrator.py
@@ -0,0 +1,62 @@
+class CameraCalibration():
+    pass
+
+    """
+    
+    """
+    def __init__(self, images):
+        #self.images = images
+        objpoints, imgpoints = get_points(images)
+        self.ret, self.mtx, self.dist, self.rvecs, self.tvecs = \
+        calibrate_camera(objpoints, imgpoints)
+        
+    
+    
+    def get_points(self, images):
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((6*9,3), np.float32)
+        objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+        # Arrays to store object points and image points from all the images.
+        objpoints = [] # 3d points in real world space
+        imgpoints = [] # 2d points in image plane.
+
+        # Make a list of calibration images
+        #images = glob.glob('camera_cal/calibration*.jpg')
+
+        # Step through the list and search for chessboard corners
+        for fname in images:
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+            # Find the chessboard corners
+            ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+            # If found, add object points, image points
+            if ret == True:
+                objpoints.append(objp)
+                imgpoints.append(corners)
+
+                # Draw and display the corners
+                img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+                #cv2.imshow('img',img)
+                #cv2.waitKey(500)
+                plt.figure()
+                plt.imshow(img)
+                plt.show()
+            else:
+                print('not found')
+        #cv2.destroyAllWindows()
+        return objpoints, imgpoints
+        
+        
+    def calibrate_camera(self, images):
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+        
+        
+    def undistort_image(objpoints, imgpoints, src_img):
+
+        img_size = (src_img.shape[1], src_img.shape[0])
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+        result = cv2.undistort(src_img, mtx, dist, None, mtx)
+        return result  
